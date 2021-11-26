904aa676dd194e2d737c384a311d416501f51a06
hanungss <hanungsoekamti@gmail.com>
2021-11-26 19:12:43 +0700

    search bar

---
+++ resources/views/pencarian.blade.php
@@ -0,0 +1,24 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Detail Data Pemohon</title>
+    <link
+        href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Kabupaten_Boyolali.png/529px-Kabupaten_Boyolali.png"" rel="
+        icon">
+</head>
+<x-app-layout>
+    <x-slot name="header">
+    </x-slot>
+    <x-slot name="slot">
+        <section>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    </x-slot>
+
+</x-app-layout>
